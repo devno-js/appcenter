be6d4cba37db5a41900ed38bb0cdf9b6d6c7a271
JounQin <admin@1stg.me>
2022-05-02 11:38:22 +0800

    refactor: extract appcenter mod.ts

---
+++ mod.ts
@@ -0,0 +1,74 @@
+export interface ReleaseInfo {
+  id: string;
+  version: string;
+  short_version: string;
+}
+
+export interface DownloadInfo {
+  download_url: string;
+}
+
+export const REDIRECT = 302;
+export const NOT_FOUND = 404;
+
+export async function appcenter(req: Request): Promise<Response> {
+  let url = new URL(req.url).pathname;
+
+  if (!url || url === '/') {
+    url = '/index.html';
+  }
+
+  if (/\.[a-z]+[a-z\d]*$/.test(url)) {
+    return new Response(await Deno.readFile(`./public${url}`));
+  }
+
+  const matched = /\/([\w-]+)\/([\w-]+)\/([.\w]+)/.exec(url);
+
+  if (!matched) {
+    return new Response('Not Found', {
+      status: NOT_FOUND,
+    });
+  }
+
+  const [, owner, app, version] = matched;
+
+  const releasesUrl =
+    `https://install.appcenter.ms/api/v0.1/apps/${owner}/${app}/distribution_groups/public/public_releases`;
+
+  console.log(`Fetching ${releasesUrl}`);
+
+  const releases: ReleaseInfo[] = await fetch(releasesUrl).then((res) =>
+    res.ok ? res.json() : []
+  );
+
+  const found = releases.find(
+    (it) => it.version === version || it.short_version === version,
+  );
+
+  if (!found) {
+    return new Response(
+      `No matched version ${version} found for ${owner}/${app}`,
+      {
+        status: NOT_FOUND,
+      },
+    );
+  }
+
+  const releaseUrl =
+    `https://install.appcenter.ms/api/v0.1/apps/${owner}/${app}/distribution_groups/public/releases/${found.id}`;
+
+  console.log(`Fetching ${releaseUrl}`);
+
+  const { download_url: downloadUrl }: DownloadInfo = await fetch(
+    releaseUrl,
+  ).then((res) => res.json());
+
+  console.log(`Redirect to ${downloadUrl}`);
+
+  return new Response(null, {
+    status: REDIRECT,
+    headers: {
+      Location: downloadUrl,
+    },
+  });
+}
